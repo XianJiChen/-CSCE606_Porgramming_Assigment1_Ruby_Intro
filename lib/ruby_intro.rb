de4bf5881546fc4ed99ee9c39b682c95f24c597c
JimmyXianJiChen <jimmyxjc@gapp.nthu.edu.tw>
2022-09-08 23:30:37 -0500

    Finished first part

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,30 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  return arr.sum();
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  if(arr.length==0)
+	return 0
+  elsif(arr.length==1)
+	return arr[0]
+  else 
+	return arr.max(2).reduce(:+)
+  end
 end
 
+require 'set'
 def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+    appeared_nums = Set[]
+    (0..arr.length-1).each do |i|
+        if(appeared_nums.include?(number-arr[i]))
+            return true;
+        else
+            appeared_nums.add(arr[i])
+        end
+    end
+    false;
 end
 
 # Part 2
